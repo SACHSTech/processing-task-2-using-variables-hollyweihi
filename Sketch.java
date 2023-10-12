0842cf36ee04c0e94b06b38219f62204d959d97f
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2023-10-12 09:36:25 -0400

    Assignment 2

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,28 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(300, 300);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 200, 0);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    stroke(128);
+    fill(255, 128, 0);
+    ellipse(width/4, height/4,
+        width/2, height/2);
+    ellipse(width/2, height/4,
+        width/2, height/2);
+    ellipse(width/4, height/2,
+        width/2, height/2);
+    ellipse(width/2, height/2,
+        width/2, height/2);
+    
+  fill(255, 0, 0);
+    ellipse(width/3, height/3,
+            width/3, height/3);
   }
-  
-  // define other methods down here.
 }
+  
